0a9a2e19f375a21d5519efa9ee2d172912467571
ArkoSammy12 <jamesvillarreal12@gmail.com>
2026-03-24 23:39:54 -0500

    Annotate internal packages with @ApiStatus.Internal

---
+++ frontend/src/main/java/io/github/arkosammy12/jemu/frontend/gui/internal/commands/package-info.java
@@ -0,0 +1,4 @@
+@ApiStatus.Internal
+package io.github.arkosammy12.jemu.frontend.gui.internal.commands;
+
+import org.jetbrains.annotations.ApiStatus;
---
+++ frontend/src/main/java/io/github/arkosammy12/jemu/frontend/gui/internal/events/package-info.java
@@ -0,0 +1,4 @@
+@ApiStatus.Internal
+package io.github.arkosammy12.jemu.frontend.gui.internal.events;
+
+import org.jetbrains.annotations.ApiStatus;
