f07872c07b63c4593f6c1331a89caed3f2dc236d
ArkoSammy12 <jamesvillarreal12@gmail.com>
2026-03-27 23:34:52 -0500

    Fix not exporting the tests package in the test source set and not allowing gson to use reflection on gson annotated test classes

--- core/src/test/java/module-info.java
+++ core/src/test/java/module-info.java
@@ -4,4 +4,9 @@ module core.test {
     requires org.junit.jupiter.api;
     requires core;
 
+    exports io.github.arkosammy12.jemu.core.test.tests;
+
+    opens io.github.arkosammy12.jemu.core.test.ssts.cdp1802 to com.google.gson;
+    opens io.github.arkosammy12.jemu.core.test.ssts.sm83 to com.google.gson;
+    opens io.github.arkosammy12.jemu.core.test.ssts.nes6502 to com.google.gson;
 }
