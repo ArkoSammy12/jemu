e61180ef08fd377b9c339ab774f58296333c5917
ArkoSammy12 <jamesvillarreal12@gmail.com>
2026-06-21 01:33:46 -0500

    RCA Studio II: Change keypad keybindings

--- app/src/main/java/io/github/arkosammy12/jemu/app/adapters/RCAStudioIIAdapter.java
+++ app/src/main/java/io/github/arkosammy12/jemu/app/adapters/RCAStudioIIAdapter.java
@@ -33,27 +33,27 @@ public class RCAStudioIIAdapter extends AbstractSystemAdapter {
     @Override
     protected @Nullable RCAStudioIIKeypad.Action getActionForKeyCode(int keyCode) {
         return switch (keyCode) {
-            case KeyEvent.VK_ALT -> RCAStudioIIKeypad.Actions.KEYPADA_0;
-            case KeyEvent.VK_Q -> RCAStudioIIKeypad.Actions.KEYPADA_1;
-            case KeyEvent.VK_W -> RCAStudioIIKeypad.Actions.KEYPADA_2;
-            case KeyEvent.VK_E -> RCAStudioIIKeypad.Actions.KEYPADA_3;
-            case KeyEvent.VK_A -> RCAStudioIIKeypad.Actions.KEYPADA_4;
-            case KeyEvent.VK_S -> RCAStudioIIKeypad.Actions.KEYPADA_5;
-            case KeyEvent.VK_D -> RCAStudioIIKeypad.Actions.KEYPADA_6;
-            case KeyEvent.VK_Z -> RCAStudioIIKeypad.Actions.KEYPADA_7;
-            case KeyEvent.VK_X -> RCAStudioIIKeypad.Actions.KEYPADA_8;
-            case KeyEvent.VK_C -> RCAStudioIIKeypad.Actions.KEYPADA_9;
+            case KeyEvent.VK_1 -> RCAStudioIIKeypad.Actions.KEYPADA_1;
+            case KeyEvent.VK_2 -> RCAStudioIIKeypad.Actions.KEYPADA_2;
+            case KeyEvent.VK_3 -> RCAStudioIIKeypad.Actions.KEYPADA_3;
+            case KeyEvent.VK_Q -> RCAStudioIIKeypad.Actions.KEYPADA_4;
+            case KeyEvent.VK_W -> RCAStudioIIKeypad.Actions.KEYPADA_5;
+            case KeyEvent.VK_E -> RCAStudioIIKeypad.Actions.KEYPADA_6;
+            case KeyEvent.VK_A -> RCAStudioIIKeypad.Actions.KEYPADA_7;
+            case KeyEvent.VK_S -> RCAStudioIIKeypad.Actions.KEYPADA_8;
+            case KeyEvent.VK_D -> RCAStudioIIKeypad.Actions.KEYPADA_9;
+            case KeyEvent.VK_X -> RCAStudioIIKeypad.Actions.KEYPADA_0;
 
-            case KeyEvent.VK_0, KeyEvent.VK_NUMPAD0 -> RCAStudioIIKeypad.Actions.KEYPADB_0;
-            case KeyEvent.VK_1, KeyEvent.VK_NUMPAD7 -> RCAStudioIIKeypad.Actions.KEYPADB_1;
-            case KeyEvent.VK_2, KeyEvent.VK_NUMPAD8 -> RCAStudioIIKeypad.Actions.KEYPADB_2;
-            case KeyEvent.VK_3, KeyEvent.VK_NUMPAD9 -> RCAStudioIIKeypad.Actions.KEYPADB_3;
-            case KeyEvent.VK_4, KeyEvent.VK_NUMPAD4 -> RCAStudioIIKeypad.Actions.KEYPADB_4;
-            case KeyEvent.VK_5, KeyEvent.VK_NUMPAD5 -> RCAStudioIIKeypad.Actions.KEYPADB_5;
-            case KeyEvent.VK_6, KeyEvent.VK_NUMPAD6 -> RCAStudioIIKeypad.Actions.KEYPADB_6;
-            case KeyEvent.VK_7, KeyEvent.VK_NUMPAD1 -> RCAStudioIIKeypad.Actions.KEYPADB_7;
-            case KeyEvent.VK_8, KeyEvent.VK_NUMPAD2 -> RCAStudioIIKeypad.Actions.KEYPADB_8;
-            case KeyEvent.VK_9, KeyEvent.VK_NUMPAD3 -> RCAStudioIIKeypad.Actions.KEYPADB_9;
+            case KeyEvent.VK_7, KeyEvent.VK_NUMPAD7 -> RCAStudioIIKeypad.Actions.KEYPADB_1;
+            case KeyEvent.VK_8, KeyEvent.VK_NUMPAD8 -> RCAStudioIIKeypad.Actions.KEYPADB_2;
+            case KeyEvent.VK_9, KeyEvent.VK_NUMPAD9 -> RCAStudioIIKeypad.Actions.KEYPADB_3;
+            case KeyEvent.VK_U, KeyEvent.VK_NUMPAD4 -> RCAStudioIIKeypad.Actions.KEYPADB_4;
+            case KeyEvent.VK_I, KeyEvent.VK_NUMPAD5 -> RCAStudioIIKeypad.Actions.KEYPADB_5;
+            case KeyEvent.VK_O, KeyEvent.VK_NUMPAD6 -> RCAStudioIIKeypad.Actions.KEYPADB_6;
+            case KeyEvent.VK_J, KeyEvent.VK_NUMPAD1 -> RCAStudioIIKeypad.Actions.KEYPADB_7;
+            case KeyEvent.VK_K, KeyEvent.VK_NUMPAD2 -> RCAStudioIIKeypad.Actions.KEYPADB_8;
+            case KeyEvent.VK_L, KeyEvent.VK_NUMPAD3 -> RCAStudioIIKeypad.Actions.KEYPADB_9;
+            case KeyEvent.VK_COMMA, KeyEvent.VK_NUMPAD0 -> RCAStudioIIKeypad.Actions.KEYPADB_0;
             default -> null;
         };
     }
